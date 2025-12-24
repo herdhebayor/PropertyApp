64576e9a54b62d18a892b2f94fa281c8d5315c8b
herdhebayor <herdhebayor22@gmail.com>
2025-12-24 19:29:20 +0100

    re-configure middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
-export {default} from "next-auth/middleware";// Protect all routes including localhost:3000/api/...
+//export {default} from "next-auth/middleware";// Protect all routes including localhost:3000/api/...
 
-export const config = {
+export default {
   matcher: ["/properties/add","/profile","/properties/saved","/messages"],
 }; //Specify protected routes
