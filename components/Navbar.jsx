6bbca3543ec4d1b1c8f9eb544990e09e78d7abb3
herdhebayor <herdhebayor22@gmail.com>
2025-12-26 00:34:38 +0100

    fix NAvbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -38,9 +38,9 @@ const Navbar = () => {
 	console.log('Provider:', providers)
 	return (
 		<nav className='bg-blue-700 border-b border-blue-500'>
-			<div className='mx-auto max-w-7xl px-2 sm-px6 lg-px-8'>
+			<div className='mx-auto w-full px-4 sm:px-6 lg:px-8 '>
 				<div className='relative flex h-20 items-center justify-between'>
-					<div className='absolute inset-y-0 left-0 flex items-center md-hidden'>
+					<div className='absolute inset-y-0 left-16 flex items-center md:hidden'>
 						{/* Mobile menu button */}
 						<button
 							type='button'
@@ -48,7 +48,7 @@ const Navbar = () => {
 							className='relative md:hidden inline-flex items-center justify-center rounded-md p-2 text-gray-400
                      hover:bg-gray-700 hover:text-white focus-outline-none focus:ring-2 focus:ring-inset focus:ring-white'
 							aria-controls='mobile-menu'
-							aria-expanded='false'
+							aria-expanded={isMobileMenuOpen ? 'true' : 'false'}
 							onClick={() => setIsMobileMenuOpen((prev) => !prev)}
 						>
 							<span className='absolute -inset-0.5'></span>
@@ -57,7 +57,7 @@ const Navbar = () => {
 							<svg
 								className='block h-6 w-6'
 								fill='none'
-								viewBox='00 24 24'
+								viewBox='0 0 24 24'
 								strokeWidth='1.5'
 								stroke='currentColor'
 								arial-hidden='true'
@@ -71,9 +71,12 @@ const Navbar = () => {
 						</button>
 					</div>
 
-					<div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
+					<div className='flex flex-1 items-center justify-between'>
 						{/* Logo */}
-						<Link className='flex shrink-0 items-center' href='/'>
+						<Link
+							className='flex shrink-0 items-center '
+							href='/'
+						>
 							<Image
 								className='w-10 h-10 rounded-full'
 								src={Logo}
@@ -106,7 +109,7 @@ const Navbar = () => {
 									<Link
 										href='/properties/add'
 										className={`${
-											pathname === '/property/add' ? 'bg-black' : ''
+											pathname === '/properties/add' ? 'bg-black' : ''
 										} text-white hover:text-white rounded-md px-3 py-2`}
 									>
 										Add Property
@@ -235,12 +238,14 @@ const Navbar = () => {
 											Saved Properties
 										</Link>
 										<button
-											className='block px-4 py-2 text-sm text-gray-700'
-											role='menuitem'
-											tabIndex='-1'
-											id='user-menu-item-2'
+											onClick={() => {
+												setIsProfileMenuOpen(false)
+												signOut()
+											}}
+											className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
 										>
-											Sign Out
+											<FaGoogle className='text-white mr-2' />
+											<span>Sign Out </span>
 										</button>
 									</div>
 								)}
@@ -252,7 +257,7 @@ const Navbar = () => {
 
 			{/* Mobile menu show/hide based on menu state */}
 			{isMobileMenuOpen && (
-				<div className='' id='mobile-menu'>
+				<div className='block' id='mobile-menu'>
 					<div className='space-y-1 px-2 pb-3 pt-2'>
 						<Link
 							href='/'
@@ -276,7 +281,7 @@ const Navbar = () => {
 							<Link
 								href='/properties/add'
 								className={`${
-									pathname === '/property/add' ? 'bg-black' : ''
+									pathname === '/properties/add' ? 'bg-black' : ''
 								} text-white hover:text-white rounded-md px-3 py-2`}
 							>
 								Add Property
@@ -288,7 +293,7 @@ const Navbar = () => {
 									setIsProfileMenuOpen(false)
 									signOut()
 								}}
-								className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+								className='flex mt-2 items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
 							>
 								<FaGoogle className='text-white mr-2' />
 								<span>Sign Out </span>
@@ -299,7 +304,7 @@ const Navbar = () => {
 								<button
 									key={index}
 									onClick={() => signIn(provider.id)}
-									className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+									className='flex mt-2 items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
 								>
 									<FaGoogle className='text-white mr-2' />
 									<span>Login or Register </span>
